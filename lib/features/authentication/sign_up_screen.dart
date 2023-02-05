11ac103bbc715e8d3c01b51d66cec2cdcd1f6b75
ikang-muse <inho.kang@muse.live>
2023-02-05 13:36:10 -0500

    feature/Birthday Screen

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/login_screen.dart';
+import 'package:tiktok_clone/features/authentication/username_screen.dart';
+import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
+
+  void _onLoginTap(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const LoginScreen(),
+    ));
+  }
+
+  void _onEmailTap(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const UsernameScreen(),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +28,9 @@ class SignUpScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
           child: Column(
-            children: const [
+            children: [
               Gaps.v80,
-              Text(
+              const Text(
                 "Sign up for TikTok",
                 style: TextStyle(
                   fontSize: Sizes.size24,
@@ -22,7 +38,7 @@ class SignUpScreen extends StatelessWidget {
                 ),
               ),
               Gaps.v20,
-              Text(
+              const Text(
                 "Create a profile, follow other accounts, make your own videos, and more.",
                 style: TextStyle(
                   fontSize: Sizes.size16,
@@ -30,12 +46,24 @@ class SignUpScreen extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
+              Gaps.v40,
+              GestureDetector(
+                onTap: () => _onEmailTap(context),
+                child: const AuthButton(
+                    text: "Use email & password",
+                    icon: FaIcon(FontAwesomeIcons.user)),
+              ),
+              Gaps.v16,
+              const AuthButton(
+                text: "Continue with Apple",
+                icon: FaIcon(FontAwesomeIcons.apple),
+              ),
             ],
           ),
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-          color: Colors.grey.shade100,
+          color: Colors.grey.shade50,
           elevation: 2,
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: Sizes.size32),
@@ -47,11 +75,14 @@ class SignUpScreen extends StatelessWidget {
                     'Already have an account?',
                   ),
                   Gaps.h5,
-                  Text(
-                    'Log in',
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.w600,
+                  GestureDetector(
+                    onTap: () => _onLoginTap(context),
+                    child: Text(
+                      'Log in',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
                 ],
---
+++ lib/features/authentication/widgets/form_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class FormButton extends StatelessWidget {
+  const FormButton({
+    super.key,
+    required this.disabled,
+  });
+
+  final bool disabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: AnimatedContainer(
+        padding: const EdgeInsets.symmetric(vertical: Sizes.size16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(Sizes.size3),
+          color:
+              disabled ? Colors.grey.shade200 : Theme.of(context).primaryColor,
+        ),
+        duration: const Duration(milliseconds: 300),
+        child: AnimatedDefaultTextStyle(
+          duration: const Duration(milliseconds: 300),
+          style: TextStyle(
+              color: disabled ? Colors.grey.shade400 : Colors.white,
+              fontWeight: FontWeight.w700),
+          child: const Text(
+            'Next',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
